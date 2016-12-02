34ecc65347fb1b866de70f08936ab1b32ccfbcba
smahade4 <smahdae4@hawk.iit.edu>
2016-12-01 23:24:27 -0600

    added cluster summarize

--- a4/summarize.py
+++ a4/summarize.py
@@ -14,28 +14,44 @@ def main():
  classifyinput = open("classifyinput.pkl","rb")
  messagedata=pickle.load(classifyinput)
  counterdata=Counter()
- counterdata.update(users['screen_name'])
- for val in users:       
+ for val in users:        
+     counterdata.update(val['screen_name'])
      counterdata.update(val['connection'])
     
- finalfile.write("Number of users collected"+len(users),finaldata)
- finalfile.write("Number of users collected"+len(counterdata),finaldata)
- finalfile.write("Number of messages collected"+len(messagedata),finaldata)
+ finalfile.write("Number of users collected "+str(len(users)))
+ finalfile.write("\n")
+ finalfile.write("Number of users collected "+str(len(counterdata)))
+ finalfile.write("\n")
+ finalfile.write("Number of messages collected "+str(len(messagedata)))
+ finalfile.write("\n")
  clusteroutput = open("clusteroutput.pkl","rb")
  clusters=pickle.load(clusteroutput)
  total=0
  for i in range(0,len(clusters)):
           total=total+len(clusters[i])
  
- finalfile.write("Number of communities discovered"+len(clusters))
- finalfile.write("Average number of users per community"+total/len(clusters))
+ finalfile.write("Number of communities discovered "+str(len(clusters)))
+ finalfile.write("\n")
+ finalfile.write("Average number of users per community "+str(total/len(clusters)))
+ finalfile.write("\n")
  classifyoutput = open("classifyoutput.pkl","rb")
  classify=pickle.load(classifyoutput)
  classifycounter=Counter()
  classifycounter.update(classify)
- finalfile.write("Number of instances for class 0 -Male found"+classifycounter[0])
- finalfile.write("Number of instances for class 1 -Female found"+classifycounter[1])
-
+ finalfile.write("Number of instances for class 0 -Male found "+str(classifycounter[0]))
+ finalfile.write("\n")
+ finalfile.write("Number of instances for class 1 -Female found "+str(classifycounter[1]))
+ finalfile.write("\n")
+ classifyinstance0 = open("classifyoutputinstance0.pkl","rb")
+ classify=pickle.load(classifyinstance0)
+ finalfile.write("Example of class 0"+str( classify[0]))
+ 
+ classifyinstance0 = open("classifyoutputinstance0.pkl","rb")
+ classify=pickle.load(classifyinstance0)
+ classifyinstance1 = open("classifyoutputinstance1.pkl","rb")
+ classify=pickle.load(classifyinstance1)
+ finalfile.write("\n") 
+ finalfile.write("Example of class 1"+str( classify[0]))
  
 if __name__ == '__main__':
     main()
